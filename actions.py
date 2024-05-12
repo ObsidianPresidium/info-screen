bf577882e0c78c3ea8729b46ffb8318c92cac6fc
ObsidianPresidium <emil@aarestrup.nu>
2024-05-12 15:32:25 +0200

    add: only git pull if not in dev environment

--- actions.py
+++ actions.py
@@ -1,7 +1,10 @@
 import os
+import socket
+
+is_dev_env = False if socket.gethostname() == "raspberrypi" else True
 
 def match_action(rfile):
-    if rfile == "action=git-pull":
+    if rfile == "action=git-pull" and not is_dev_env:
         git_pull()
 
 def git_pull():
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from webserver import webserver
 import threading
-import os
+import socket
+
+is_dev_env = False if socket.gethostname() == "raspberrypi" else True
 
 server_thread = threading.Thread(target=webserver)
 server_thread.start()
