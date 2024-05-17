c797b33de3f09a6db2dd558363bead0ad00d4cd3
ObsidianPresidium <emil@aarestrup.nu>
2024-05-17 22:36:29 +0200

    fix: runtime-data being referenced before being created

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import subprocess
 import sys
+import os
 from func import preprocess, get_output
 
 print("Writing host version")
+if not os.path.exists("runtime-data"):
+    os.mkdir("runtime-data")
 with open("runtime-data/last-update.txt", "w", encoding="utf-8") as f:
     f.write(get_output("git rev-parse --short HEAD"))
 
